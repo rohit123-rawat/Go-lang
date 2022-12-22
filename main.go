165bab2c638061a36814795d42644ca0be9414e1
Rohit Rawat <rohit.rawat@coloredcow.in>
2022-12-22 16:33:38 +0530

    failing because go root is not working

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
+
 package main
 
 import (
-
+  	"github.com/gin-gonic/gin"
+	  controllers "GO-LANG/controllers"
 )
 
 func main() {
-	print("hello world! Hii")
+
+	r := gin.Default()
+	
+	public := r.Group("/api")
+
+	public.POST("/register", controllers.Register)
+
+	r.Run(":8080")
+
 }
